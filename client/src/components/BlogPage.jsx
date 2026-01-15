1f66298107eeaf9a1c871ef9b64d7b00835a632f
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2026-01-15 16:31:42 +0000

    build: add form to BlogPage

--- client/src/components/BlogPage.jsx
+++ client/src/components/BlogPage.jsx
@@ -1,7 +1,35 @@
+import {useState} from 'react'
+
 export default function BlogPage(){
+    
+    const [formValues, setFormValues] = useState({
+        name: '',
+        post: '',
+    })
+
+    function handleInputChange(e){
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleFormSubmit(e){
+        e.preventDefault();
+    }
+    
     return(
         <>
-            <h2>Blog Posts</h2>      
+            <h2>Blog Posts</h2>
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor="name">Name: </label>
+                <input type="text" name='name' value={formValues.staffname} required onChange={handleInputChange}/>
+
+                <label htmlFor="post">Email: </label>
+                <input type="text" name='post' value={formValues.email} required onChange={handleInputChange}/>
+                
+                <button type='submit'>Submit</button>
+            </form>      
         </>
     )
 }
